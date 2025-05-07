41d046a3fa7a47b21ef896eff4cc6b38e8de677a
OliveiraPedro09 <pedromartinsoliveira9@gmail.com>
2025-05-07 11:37:10 -0300

    fix: RF08 -  #DP2 - Conexão Mongo

--- sub_simulator.py
+++ sub_simulator.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 
-broker = "localhost"
+broker = "test.mosquitto.org"  # ? as vezes o mosquitto não está disponível usar localhost
 port = 1883
 topic = "/roger/teste"
 
